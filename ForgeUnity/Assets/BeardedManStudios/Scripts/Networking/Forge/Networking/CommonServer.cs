d9fb52a3ef4ba7baf47aa4e29c4645f57ae526d6
Peter Halasz <skinn3r@gmail.com>
2019-08-28 20:12:27 +0100

    Minor code refactoring and cleanup.

--- ForgeUnity/Assets/BeardedManStudios/Scripts/Networking/Forge/Networking/CommonServer.cs
+++ ForgeUnity/Assets/BeardedManStudios/Scripts/Networking/Forge/Networking/CommonServer.cs
@@ -14,6 +14,15 @@ namespace BeardedManStudios.Forge.Networking
 			this.server = server;
 		}
 
+		/// <summary>
+		/// Check if the target <see cref="NetworkingPlayer"/> should receive the <see cref="FrameStream"/> message
+		/// </summary>
+		/// <param name="player">The target <see cref="NetworkingPlayer"/> to send the <see cref="FrameStream"/> message to</param>
+		/// <param name="frame">The <see cref="FrameStream"/> message to send</param>
+		/// <param name="proximityDistance">The proximity distance to use for checking.</param>
+		/// <param name="skipPlayer">The <see cref="NetworkingPlayer"/> to skip</param>
+		/// <param name="proximityModeUpdateFrequency">The frequency of proximity updates (number of updates to skip before sending one)</param>
+		/// <returns>True if the target <see cref="NetworkingPlayer"/> should receive the message, false otherwise</returns>
 		public bool PlayerIsReceiver(NetworkingPlayer player, FrameStream frame, float proximityDistance, NetworkingPlayer skipPlayer = null, int proximityModeUpdateFrequency = 0)
 		{
 			// Don't send messages to a player who has not been accepted by the server yet
@@ -35,6 +44,15 @@ namespace BeardedManStudios.Forge.Networking
 			return true;
 		}
 
+		/// <summary>
+		/// Checks if target <see cref="NetworkingPlayer"/> should receive a proximity based <see cref="FrameStream"/> message
+		/// </summary>
+		/// <param name="sender">The sending <see cref="NetworkingPlayer"/></param>
+		/// <param name="player">The target <see cref="NetworkingPlayer"/></param>
+		/// <param name="frame">The <see cref="FrameStream"/> message</param>
+		/// <param name="proximityDistance">The proximity distance to use for the check</param>
+		/// <param name="proximityModeUpdateFrequency">The update frequency proximity messages should be sent at (number of updates to skip before sending one)</param>
+		/// <returns></returns>
 		public bool PlayerIsDistanceReceiver(NetworkingPlayer sender, NetworkingPlayer player, FrameStream frame, float proximityDistance, float proximityModeUpdateFrequency)
 		{
 			// check for distance here so the owner doesn't need to be sent in stream, used for NCW field proximity check
@@ -42,32 +60,35 @@ namespace BeardedManStudios.Forge.Networking
 			{
 				if ((frame.Receivers == Receivers.AllProximity || frame.Receivers == Receivers.OthersProximity))
 				{
-					return proximityDistanceCheck(sender, player, proximityDistance, proximityModeUpdateFrequency);
+					return ProximityDistanceCheck(sender, player, proximityDistance, proximityModeUpdateFrequency);
 				}
 				else if((frame.Receivers == Receivers.AllProximityGrid || frame.Receivers == Receivers.OthersProximityGrid))
 				{
-					return proximityGridCheck(sender, player, proximityDistance, proximityModeUpdateFrequency);
+					return ProximityGridCheck(sender, player, proximityDistance, proximityModeUpdateFrequency);
 				}
 			}
 			return true;
 		}
 
-		private bool proximityDistanceCheck(NetworkingPlayer sender, NetworkingPlayer player, float proximityDistance, float proximityModeUpdateFrequency)
+		/// <summary>
+		/// Check if the target player is in proximity distance to the sender.
+		/// </summary>
+		/// <param name="sender">The sending <see cref="NetworkingPlayer"/></param>
+		/// <param name="player">The target <see cref="NetworkingPlayer"/></param>
+		/// <param name="proximityDistance">The proximity distance the target needs to be from the sender to receive an update</param>
+		/// <param name="proximityModeUpdateFrequency">The frequency of updates to be sent to the target if in proximity (number of updates to skip before sending one)</param>
+		/// <returns>True if the target player is in proximity distance, otherwise false</returns>
+		private bool ProximityDistanceCheck(NetworkingPlayer sender, NetworkingPlayer player, float proximityDistance, float proximityModeUpdateFrequency)
 		{
-			// If the target player is not in the same proximity zone as the sender
-			// then it should not be sent to that player
-			if (player.ProximityLocation.DistanceSquared(sender.ProximityLocation) > proximityDistance * proximityDistance)
-			{
-				// if update frequency is 0, it shouldn't ever get updated while too far
-				if (proximityModeUpdateFrequency == 0)
-					return false;
+			// If the target player is in proximity distance the send them the frame
+			if (IsInProximityDistance(player.ProximityLocation, sender.ProximityLocation, proximityDistance))
+				return true;
 
-				return updateCountCheck(sender, player, proximityModeUpdateFrequency);
-			}
-			return true;
+			// if update frequency is 0, it shouldn't ever get updated while too far otherwise send update based on frequency
+			return proximityModeUpdateFrequency != 0 && ProximityUpdateCountCheck(sender, player, proximityModeUpdateFrequency);
 		}
 
-		private bool proximityGridCheck(NetworkingPlayer sender, NetworkingPlayer player, float proximityDistance, float proximityModeUpdateFrequency)
+		private bool ProximityGridCheck(NetworkingPlayer sender, NetworkingPlayer player, float proximityDistance, float proximityModeUpdateFrequency)
 		{
 			// If the target player is not in the same proximity grid zone as the sender
 			// then it should not be sent to that player
@@ -77,13 +98,32 @@ namespace BeardedManStudios.Forge.Networking
 				if (proximityModeUpdateFrequency == 0)
 					return false;
 
-				return updateCountCheck(sender, player, proximityModeUpdateFrequency);
+				return ProximityUpdateCountCheck(sender, player, proximityModeUpdateFrequency);
 
 			}
 			return true;
 		}
 
-		private bool updateCountCheck(NetworkingPlayer sender, NetworkingPlayer player, float proximityModeUpdateFrequency)
+		/// <summary>
+		/// Check if the target location <see cref="Vector"/> and the source <see cref="Vector"/>'s distance is less then or equal to the proximity distance
+		/// </summary>
+		/// <param name="target"></param>
+		/// <param name="source"></param>
+		/// <param name="proximityDistance"></param>
+		/// <returns></returns>
+		private bool IsInProximityDistance(Vector target, Vector source, float proximityDistance)
+		{
+			return target.DistanceSquared(source) <= proximityDistance * proximityDistance;
+		}
+
+		/// <summary>
+		/// Check to see if a far away <see cref="NetworkingPlayer"/> should still receive an update.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="player"></param>
+		/// <param name="proximityModeUpdateFrequency"></param>
+		/// <returns></returns>
+		private bool ProximityUpdateCountCheck(NetworkingPlayer sender, NetworkingPlayer player, float proximityModeUpdateFrequency)
 		{
 			// if player update counts are stored, increment or update and reset them, if not, store them starting with 0
 			string key = player.Ip + player.NetworkId.ToString();
